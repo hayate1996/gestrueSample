ebd18d5e0e8e9621ae4c4e93a24360af15c2ea75
NAGAMINE HIROMASA <JP11762@w022381403486m.local>
2015-01-19 12:54:54 +0900

    Initial Commit

---
+++ GestureSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GestureSample
+//
+//  Created by NAGAMINE HIROMASA on 2015/01/19.
+//  Copyright (c) 2015年 Hiromasa Nagamine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
